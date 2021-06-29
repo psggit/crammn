ef4dc398af764a3fe4aa26e454c7ec5581aacace
Y Ramesh Rao <yrameshrao@gmail.com>
2021-06-29 07:05:45 +0530

    Client Setup Completed

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # CRAMMN.COM
+
 World-class Peer-2-Peer college networking platform
 
 # Installation Gotchas
-* Using module and ES6 constructs, make sure node 12 and above is installed
+
+- Using module and ES6 constructs, make sure node 12 and above is installed
--- server/index.js
+++ server/index.js
@@ -1,21 +1,25 @@
-import express from 'express'
-import fs from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import express from "express"
+import fs from "fs"
+import path from "path"
+import { fileURLToPath } from "url"
+import { dirname } from "path"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-const PORT = process.env.port || 3001;
-const app = express();
+const PORT = process.env.port || 3001
+const app = express()
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")))
 
-app.listen(PORT, ()=>{
-    let bannerPath = path.join(__dirname, '/banner.txt');
-    process.stdout.write(fs.readFileSync(bannerPath, {encoding:'utf8', flag:'r'}))
-    process.stdout.write('\n************************************************************\n')
-    process.stdout.write(`SERVER STARTED @ ${PORT}\n`)
-    process.stdout.write('************************************************************\n')
+app.listen(PORT, () => {
+  let bannerPath = path.join(__dirname, "/banner.txt")
+  process.stdout.write(fs.readFileSync(bannerPath, { encoding: "utf8", flag: "r" }))
+  process.stdout.write(
+    "\n************************************************************\n"
+  )
+  process.stdout.write(`SERVER STARTED @ ${PORT}\n`)
+  process.stdout.write(
+    "************************************************************\n"
+  )
 })
