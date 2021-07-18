e2104ef40aa9b73928587f0737d1812c8133e6a0
Y Ramesh Rao <yrameshrao@gmail.com>
2021-07-18 11:37:50 +0000

    Squashed commit of the following
    commit b1b8a858 
    Author: Ramesh <Ramesh> 
    Date: Sun Jul 18 2021 17:07:27 GMT+0530 (India Standard Time) 
    
        Conflicts resolved in the console.
    
    commit 53cc8048 
    Author: Y Ramesh Rao <yrameshrao@gmail.com> 
    Date: Sun Jul 18 2021 17:04:11 GMT+0530 (India Standard Time) 
    
        Merge branch 'master' of ssh://git-codecommit.ap-south-1.amazonaws.com/v1/repos/crammn-app
    
    
    commit 0316d542 
    Author: Y Ramesh Rao <yrameshrao@gmail.com> 
    Date: Sun Jul 18 2021 17:03:53 GMT+0530 (India Standard Time) 
    
        Production Client JS Generated
    
    
    commit 750a7b71 
    Author: Ramesh <Ramesh> 
    Date: Sun Jul 18 2021 16:36:51 GMT+0530 (India Standard Time) 
    
        Conflicts resolved in the console.
    
    commit d9cf1a8d 
    Author: Y Ramesh Rao <yrameshrao@gmail.com> 
    Date: Sun Jul 18 2021 17:01:30 GMT+0530 (India Standard Time) 
    
        Production Client JS Generated
    
    
    commit 82695f3c 
    Author: Y Ramesh Rao <yrameshrao@gmail.com> 
    Date: Sun Jul 18 2021 16:34:39 GMT+0530 (India Standard Time) 
    
        Production Client JS Generated
    
    
    commit 3eae48e8 
    Author: Y Ramesh Rao <yrameshrao@gmail.com> 
    Date: Sun Jul 18 2021 17:00:23 GMT+0530 (India Standard Time) 
    
        Sign-in Localhost bug fix
    
    
    commit 6fa4b2f2 
    Author: Y Ramesh Rao <yrameshrao@gmail.com> 
    Date: Sun Jul 18 2021 16:31:55 GMT+0530 (India Standard Time) 
    
        Formatting Changes
    
    
    commit 8563b988 
    Author: Y Ramesh Rao <yrameshrao@gmail.com> 
    Date: Sun Jul 18 2021 16:31:29 GMT+0530 (India Standard Time) 
    
        Mobile Header Dom Manipulations Added
    
    
    commit 1358c08b 
    Author: Ramesh <Ramesh> 
    Date: Thu Jul 15 2021 12:49:39 GMT+0530 (India Standard Time) 
    
        Conflicts resolved in the console.
    
    commit 57b84061 
    Author: cyanoprem <cyano.prem@gmail.com> 
    Date: Thu Jul 15 2021 11:28:50 GMT+0530 (India Standard Time) 
    
        Changed the title to Cram Smarter

--- client/src/Signin.jsx
+++ client/src/Signin.jsx
@@ -1,23 +1,24 @@
 import React from "react"
 import { ErrorMessage, Field, Form, Formik } from "formik"
-import { navigate } from "@reach/router"
+import {navigate} from "@reach/router";
+import GoogleSignIn from "./GoogleSignIn"
 
 const SignIn = () => {
-  React.useEffect(() => {
-    console.log("LOADING DATA")
-    console.log(`/api/user/details/${window.location.search}`)
-    fetch(`/api/user/details/`)
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.profileUpdated) {
-          return navigate(`/courses`)
-        }
-        if (data.authorized) {
-          return navigate(`/signup`)
-        }
-        return data
-      })
-  }, [])
+    React.useEffect(() => {
+        console.log("LOADING DATA")
+        console.log(`/api/user/details/${window.location.search}`)
+        fetch(`/api/user/details/`)
+            .then((res) => res.json())
+            .then(data=>{
+                if(data.profileUpdated){
+                    return navigate(`/courses`)
+                }
+                if(data.authorized){
+                    return navigate(`/signup`)
+                }
+                return data
+            })
+    }, [])
 
   return (
     <main data-testid="main">
@@ -31,26 +32,7 @@ const SignIn = () => {
         <div className="col-lg-5">&nbsp;</div>
 
         <div className="col-lg-3 mt-5">
-          <div
-            id="g_id_onload center"
-            data-client_id="86586798573-o43mavsuo9aclh25qb2jn9vkcnpo63bt.apps.googleusercontent.com"
-            data-context="use"
-            data-ux_mode="popup"
-            data-login_uri="http://localhost:3001/auth/google/callback"
-            data-nonce=""
-            data-auto_prompt="false"
-          ></div>
-
-          <div
-            className="g_id_signin center"
-            data-type="standard"
-            data-shape="pill"
-            data-theme="filled_blue"
-            data-text="signin_with"
-            data-size="large"
-            data-logo_alignment="left"
-            data-width="300px"
-          ></div>
+          <GoogleSignIn />
         </div>
       </section>
     </main>
