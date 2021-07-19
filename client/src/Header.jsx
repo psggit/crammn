753d0ff10578dded9b3b6eb991e8d17b9863f5bf
Y Ramesh Rao <yrameshrao@gmail.com>
2021-07-19 21:44:12 +0530

    Conflicts Fix

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,19 +1,30 @@
-import React from "react"
-import {Link, useParams} from "@reach/router";
+import React, { useRef, useState } from "react"
+import { Link } from "@reach/router"
 import GoogleSignIn from "./GoogleSignIn"
 
-const NavLink = props => {
+const safeCheck = (fn) => {
+  try {
+    return fn()
+  } catch (err) {
+    console.error(err)
+    // no_op
+  }
+  return false
+}
 
-    return (
-        <Link{...props} getProps={({isCurrent}) => {
-            // the object returned here is passed to the
-            return {
-                className: isCurrent ? 'active' : ''
-            };
+const NavLink = (props) => {
+  return (
+    <Link
+      {...props}
+      getProps={({ isCurrent }) => {
+        // the object returned here is passed to the
+        return {
+          className: isCurrent ? "active" : "",
         }
-        }
-        />)
-};
+      }}
+    />
+  )
+}
 
 const Header = (props) => {
   const [userData, setUserData] = useState(null)
@@ -24,88 +35,86 @@ const Header = (props) => {
   React.useEffect(() => {
     console.log("LOADING DATA")
 
-    const [userData, setUserData] = React.useState(null)
+    fetch(`/api/user/details/`)
+      .then((res) => res.json())
+      .then((data) => setUserData(data))
+  }, [])
 
-    React.useEffect(() => {
-        console.log("LOADING DATA")
-        fetch(`/api/user/details/`)
-            .then((res) => res.json())
-            .then((data) => setUserData(data))
-    }, [])
-
-    const startLearning = () => {
-        if (userData && userData.authorized)
-            return null
-
-        return (
-            <li id='signindropdown' className="dropdown sign-dropdown">
-                    <a id="get-started-btn" href="#" className="dropdown get-started-btn dropdown">Start Learning <i
-                        className="bi bi-chevron-down"></i></a>
-                    <ul className="sign-dropdown-container">
-                        <div id="g_id_onload"
-                             data-client_id="86586798573-o43mavsuo9aclh25qb2jn9vkcnpo63bt.apps.googleusercontent.com"
-                             data-context="use"
-                             data-ux_mode="popup"
-                             data-login_uri="https://crammn.com/auth/google/callback"
-                             data-nonce=""
-                             data-auto_prompt="false">
-                        </div>
-
-                        <div className="g_id_signin center"
-                             data-type="standard"
-                             data-shape="pill"
-                             data-theme="filled_blue"
-                             data-text="signin_with"
-                             data-size="large"
-                             data-logo_alignment="left"
-                             data-width="300px">
-                        </div>
-                    </ul>
-            </li>)
-    }
-
-    const signOut = () => {
-        if (userData && userData.authorized)
-            return (
-                <li className="dropdown sign-dropdown">
-                    <a id="get-started-btn" href="#" className="dropdown get-started-btn dropdown">Account <i
-                        className="bi bi-chevron-down"></i></a>
-                    <ul>
-                        <li>
-                            <a href="/signout">Sign me out!!</a>
-                        </li>
-                    </ul>
-                </li>)
-
-        return null
-    }
-
+  const startLearning = () => {
+    if (userData && userData.authorized) return null
 
     return (
-        <header id="header" className="fixed-top">
-            <div className="container d-flex align-items-center">
-
-                <h1 className="logo me-auto"><a href="index.html">Crammn</a></h1>
-
-                <nav id="navbar" className="navbar order-last order-lg-0">
-                    <ul>
-                        <li><NavLink to='/'>Home</NavLink></li>
-                        <li><NavLink to='/about'>About</NavLink></li>
-                        <li><NavLink to='/courses'>Courses</NavLink></li>
-                        <li><NavLink to='/contact'>Contact</NavLink></li>
-                        {startLearning()}
-                        {signOut()}
-                    </ul>
-
-                        <i className="bi bi-list mobile-nav-toggle"></i>
-                </nav>
-
-            </div>
-        </header>
+      <li id="signindropdown" className="dropdown sign-dropdown ">
+        <a id="get-started-btn" href="#" className="dropdown get-started-btn dropdown" onClick={() => setMobileSignInActive(!isMobileSignInActive)}>
+          Start Learning <i className="bi bi-chevron-down"></i>
+        </a>
+        <ul className="sign-dropdown-container" className={isMobileSignInActive ? "dropdown-active" : ""}>
           <GoogleSignIn />
         </ul>
       </li>
     )
-    }
+  }
 
-    export default Header;
+  const signOut = () => {
+    if (userData && userData.authorized)
+      return (
+        <li className="dropdown sign-dropdown">
+          <a id="get-started-btn" href="#" className="dropdown get-started-btn dropdown" onClick={() => setMobileSignOutActive(!isMobileSignOutActive)}>
+            Account <i className="bi bi-chevron-down"></i>
+          </a>
+          <ul className={isMobileSignOutActive ? "dropdown-active" : ""}>
+            <li>
+              <a href="/signout">Sign me out!!</a>
+            </li>
+          </ul>
+        </li>
+      )
+
+    return null
+  }
+
+  return (
+    <header id="header" className="fixed-top">
+      <div className="container d-flex align-items-center">
+        <h1 className="logo me-auto">
+          <NavLink to="/">Crammn</NavLink>
+        </h1>
+
+        <nav
+          id="navbar"
+          className={isMobileNav ? "navbar order-last order-lg-0 navbar-mobile" : "navbar order-last order-lg-0"}
+          onClick={(e) => {
+            if (safeCheck(() => mobileNavRef.current.contains(e.target))) {
+              if (isMobileNav && !safeCheck(() => e.target.classList.contains("get-started-btn"))) setMobileNav(false)
+              return
+            }
+            if (isMobileNav) setMobileNav(false)
+            if (isMobileSignInActive) setMobileSignInActive(false)
+            if (isMobileSignOutActive) setMobileSignOutActive(false)
+          }}
+        >
+          <ul ref={mobileNavRef}>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/courses">Courses</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+            {startLearning()}
+            {signOut()}
+          </ul>
+
+          <i className="bi bi-list mobile-nav-toggle" onClick={() => setMobileNav(!isMobileNav)}></i>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
