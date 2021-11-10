8dc052c681500bef9fda00fbcfff3dd7a242da17
Cyano <cyano.prem@gmail.com>
2021-11-10 07:24:34 +0000

    Updated contact details

--- client/src/Contact.jsx
+++ client/src/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => (
       <div data-aos="fade-up">
         <iframe
           style={{ border: 0, width: "100%", height: "350px" }}
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31111.684304945866!2d77.6283130122391!3d12.910257867339409!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1491bfdc6ecd%3A0xf232718439fbc879!2sHSR%20Layout%2C%20Bengaluru%2C%20Karnataka!5e0!3m2!1sen!2sin!4v1620635589355!5m2!1sen!2sin"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.057347523747!2d77.6464924138429!3d12.904034019851021!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae15d4d153d93d%3A0x7f4ba39771b3d9a2!2sIndiQube%20Orion!5e0!3m2!1sen!2sin!4v1636528946451!5m2!1sen!2sin"
           frameborder="0"
           allowfullscreen
         ></iframe>
@@ -25,18 +25,18 @@ const Contact = () => (
               <div class="address">
                 <i class="bi bi-geo-alt"></i>
                 <h4>Location:</h4>
-                <p>HSR Layout, Bangalore, Karnataka 560102</p>
+                <p>CollectiveBloom Technologies Pvt Ltd, IndiQube Orion, 24th Main Road, HSR Layout, Bangalore, Karnataka 560102</p>
               </div>
 
               <div class="email">
                 <i class="bi bi-envelope"></i>
                 <h4>Email:</h4>
-                <p>cyano.prem@gmail.com</p>
+                <p>office@crammn.com</p>
               </div>
 
               <div class="phone">
                 <i class="bi bi-phone"></i>
-                <h4>WhataApp Message @:</h4>
+                <h4>WhatsApp Message @:</h4>
                 <p>+91 9524227932</p>
               </div>
             </div>
